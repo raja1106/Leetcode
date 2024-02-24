12d87f03416f254ce6833a43e6afa9326ec942c8
Raja <raja2021sekar@gmail.com>
2024-02-24 13:40:13 -0600

    Feb24

--- arrays/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
+++ arrays/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.py
@@ -1,26 +1,46 @@
 import heapq
+import collections.deque
+from sortedcontainers import SortedList
 
 class Solution:
     #Input: nums = [8,2,4,7], limit = 4
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         left=0
         max_length=0
-        min_heap=[]
-        max_heap=[]
+        minQueue, maxQueue = collections.deque(), collections.deque()
         for i in range(len(nums)):
 
-            heapq.heappush(min_heap,nums[i])
-            heapq.heappush(max_heap, -nums[i])
-            min_value = min_heap[0]
-            max_value = -max_heap[0]
+            while minQueue and minQueue[-1] > nums[i]:
+                minQueue.pop()
+            minQueue.append(nums[i])
 
-            while left <= i and max_value-min_value > limit:
-                heapq.heapreplace()
-                min_heap.pop(nums[left])
-                max_heap.pop(nums[left])
-                min_value = min_heap[0]
-                max_value = -max_heap[0]
+            while maxQueue and maxQueue[-1] < nums[i]:
+                maxQueue.pop()
+            maxQueue.append(nums[i])
+
+            while left <= i and maxQueue[0]-minQueue[0] > limit:
+                if nums[left] == minQueue[0]:
+                    minQueue.popleft()
+                if nums[left] == maxQueue[0]:
+                    maxQueue.popleft()
                 left += 1
 
             max_length = max(max_length,i-left+1)
+        return max_length
+
+    def longestSubarrayUsingSOrtedList(self, nums: List[int], limit: int) -> int:
+        sorted_list = SortedList()
+        max_length=0
+        left=0
+
+        for i,value in enumerate(nums):
+            sorted_list.add(value)
+
+            while left <= i and sorted_list[-1]-sorted_list[0] > limit:
+
+                sorted_list.remove(nums[left])
+                left += 1
+
+            max_length =max(max_length,i-left+1)
+
         return max_length
