b6e7f3471dd1a70de4506d9cd16c8db64a30af91
Raja <raja2021sekar@gmail.com>
2024-02-28 20:02:10 -0600

    Feb27

---
+++ sorting/Bubble Sorting.py
@@ -0,0 +1,14 @@
+from typing import List
+class BubbleSort:
+    def bubbleSort(self, array: List[int]) -> int:
+
+        for i in range(len(array)):
+            for j in range(len(array) - 1, i, -1):
+                if array[j - 1] > array[j]:
+                    array[j - 1], array[j] = array[j], array[j - 1]
+        return array
+
+
+
+sort =BubbleSort()
+print(sort.bubbleSort([4,-6,1,2,3,-5]))
